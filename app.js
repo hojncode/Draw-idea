08da29715daf98025f4087760dde9006c05642b3
hojncode <glicm12@gmail.com>
2022-12-24 18:11:59 +0900

    .moveTo & .lineTo

--- app.js
+++ app.js
@@ -7,16 +7,25 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-//fillRect 메소드 : x,y,width,height ++ strokeRect를 사용하면 테두리만 있는 사각형을 만듬.
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
+//moveTo 해당 위치로 시작점을 옮긴다.
+ctx.moveTo(50, 50);
+//lineTo 해당 위치까지 선을 그린다.
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
 ctx.fill();
-//beginPath 이전 경로를 종료하고 새로운 경로를 시작함.
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
-setTimeout(() => {
-  ctx.fill();
-}, 3000);
+
+// //fillRect 메소드 : x,y,width,height ++ strokeRect를 사용하면 테두리만 있는 사각형을 만듬.
+// ctx.rect(50, 50, 100, 100);
+// ctx.rect(150, 150, 100, 100);
+// ctx.rect(250, 250, 100, 100);
+// ctx.fill();
+// //beginPath 이전 경로를 종료하고 새로운 경로를 시작함.
+// ctx.beginPath();
+// ctx.rect(350, 350, 100, 100);
+// ctx.rect(450, 450, 100, 100);
+// ctx.fillStyle = "red";
+// setTimeout(() => {
+//   ctx.fill();
+// }, 3000);
